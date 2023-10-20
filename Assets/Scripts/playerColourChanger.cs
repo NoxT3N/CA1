c264e6a17b60483f6992291b11d6e479669dc60a
Felix <145358483+NoxT3N@users.noreply.github.com>
2023-10-20 19:42:18 +0100

    Added different colored coin prefabs, when in contact with a coin of different color the player can now change color

---
+++ Assets/Scripts/playerColourChanger.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using UnityEngine;
+
+public class playerColourChanger : MonoBehaviour
+{
+    [HideInInspector]public Color pColour;
+    private SpriteRenderer playerRender;
+
+    void Awake(){
+        playerRender = GetComponent<SpriteRenderer>();
+        pColour = playerRender.color;    
+
+    }
+
+    //when player enters a trigger collider it checks if that collider is tagged as coin
+    //if yes then it gets the colour of the coin through its sprite render component
+    //and check if it's the same with the player's colour
+    //if not then the player's colour is changed to the coins colour 
+    //through the sprite renderer component and the coin gets detroyed 
+    private void OnTriggerEnter2D(Collider2D other){
+        if(other.gameObject.CompareTag("Coin")){
+            Color coinColour = other.GetComponent<SpriteRenderer>().color;
+            if(pColour != coinColour){
+                playerRender.material.color = coinColour;
+                Destroy(other.gameObject);
+            }
+            
+        }
+    }
+}
